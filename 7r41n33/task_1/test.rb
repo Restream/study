123fb828671477fdfad16f67c646dbc90b9a8748
Andreww8xx8 <avk@8xx8.ru>
2016-05-26 16:42:33 +0400

    Remove wrong

--- 7r41n33/task_1/test.rb
+++ 7r41n33/task_1/test.rb
@@ -3,6 +3,6 @@ require_relative './solution.rb'
 
 class User7r41n33::Task1::Test < Minitest::Test
   def test_works
-    assert { User7r41n33::Task1::Solution.it_works }
+    assert User7r41n33::Task1::Solution.it_works
   end
 end
