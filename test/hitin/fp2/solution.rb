660a0f9eeb5218ade7e96e9c312b370e729a1e68
Hitin <phoenix-h@mail.ru>
2016-05-29 14:17:27 +0400

    Homework 1,2,3

--- test/hitin/fp2/solution.rb
+++ test/hitin/fp2/solution.rb
@@ -6,18 +6,32 @@ module Hitin
 
       # Написать свою функцию my_each
       def my_each
+        for i in self
+          yield(i)
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        result = MyArray.new([])
+        my_each { |i| result << yield(i) }
+        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        result = MyArray.new
+        my_each { |i| result << i if i }
+        result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(sum = nil)
+        arr_copy = MyArray.new(self)
+        sum = sum.nil? ? arr_copy.shift : sum
+        arr_copy.my_each { |i| sum = yield(i, sum) }
+        sum
       end
     end
   end
