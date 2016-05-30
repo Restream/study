cf8e0bb337e64b477ed3ce1abadd8d8950d49cd5
Makapoxa <makarocha@gmail.com>
2016-05-30 14:31:51 +0400

    add binsearch

--- test/makapoxa/arrays/solution.rb
+++ test/makapoxa/arrays/solution.rb
@@ -5,8 +5,17 @@ module Makapoxa
         array
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, left = 0, right = array.length - 1)
+        return -1 if left > right
+        mid = (left + right) / 2
+        if array[mid] == query
+          return mid
+        elsif array[mid] > query
+          right = mid - 1
+        else
+          left = mid + 1
+        end
+        search(array, query, left, right)
       end
     end
   end
