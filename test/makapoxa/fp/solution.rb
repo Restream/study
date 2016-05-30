e4ab7457a109da0b62d5f8216c27857b277480f4
Makapoxa <makarocha@gmail.com>
2016-05-30 23:21:13 +0400

    add dots in names by year counter
    add average imdb rating counter by country, including few countries

--- test/makapoxa/fp/solution.rb
+++ test/makapoxa/fp/solution.rb
@@ -4,12 +4,14 @@ module Makapoxa
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array, _country)
-        0
+      def rating(array, country)
+        selection = array.select { |film| !film['country'].nil? && film['country'].include?(country) && !film['rating_imdb'].to_f.zero? }
+                         .map { |film| film['rating_imdb'].to_f }
+        selection.reduce(:+) / selection.length
       end
 
-      def chars_count(_films, _year)
-        0
+      def chars_count(films, year)
+        films.select { |film| film['year'].to_i == year }.map { |film| film['name'] }.reduce(0) { |a, e| a + e.scan(/\./).count }
       end
     end
   end
