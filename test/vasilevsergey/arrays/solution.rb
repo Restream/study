56cca63127dd1e35e6942f4c919ccfd78547e014
Sergey Vasilev <seruojka@gmail.com>
2016-05-31 11:44:31 +0300

    added solution for test

--- test/vasilevsergey/arrays/solution.rb
+++ test/vasilevsergey/arrays/solution.rb
@@ -2,11 +2,20 @@ module Vasilevsergey
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.map { |elem| elem > 0 ? max : elem }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, left = 0, right = array.length - 1)
+        return -1 if left > right
+        middle = (left + right) / 2
+        return middle if array[middle] == query
+        if array[middle] > query
+          right = middle - 1
+        else
+          left = middle + 1
+        end
+        search(array, query, left, right)
       end
     end
   end
