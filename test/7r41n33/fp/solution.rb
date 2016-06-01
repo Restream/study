4036a5525b50059c4fcc0d8a8c6c019f8eca37fd
Anton Chepenko <chepenkoanton2011@yandex.ru>
2016-06-01 17:11:03 +0300

    fp: rating and chars_count
    Test result fixed
    
    Tests fixed
    
    rubocop refactoring
    
    rubocop refactoring
    
    fp: refactoring rating

--- test/7r41n33/fp/solution.rb
+++ test/7r41n33/fp/solution.rb
@@ -4,12 +4,17 @@ module User7r41n33
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["al"], film["country"]
-      def rating(_films, _year)
-        0
+      def rating(films, year)
+        compact_films = films.map { |film| film if film['year'].to_i == year && film['rating_kinopoisk'].to_f > 0 }
+                             .compact
+        total = compact_films.reduce(0) { |a, e| a + e['rating_kinopoisk'].to_f }
+        (total / compact_films.size).to_f
       end
 
-      def chars_count(_films, _access_level)
-        0
+      def chars_count(films, access_level)
+        films.reduce(0) do |count, film|
+          film['access_level'] == access_level ? count + film['name'].count('а') : count
+        end
       end
     end
   end
