14ff04bf620ffa999045fb061c69ad8416744bd4
Maksim Slepukhov <maksim.slepukhov@gmail.com>
2016-06-01 15:54:38 +0400

    Implement rating and chars_count methods

--- test/amxsv/fp/solution.rb
+++ test/amxsv/fp/solution.rb
@@ -4,12 +4,16 @@ module Amxsv
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array, _access_level)
-        0
+      def rating(array, access_level)
+        result_array = array.select { |film| film['access_level'] == access_level && !film['rating_kinopoisk'].to_f.zero? }
+                            .map { |film| film['rating_kinopoisk'].to_f }
+        result_array.reduce(:+) / result_array.size
       end
 
-      def chars_count(_films, _year)
-        0
+      def chars_count(films, year)
+        films.reduce(0) do |acc, film|
+          (film['year'].to_i == year) ? (acc + film['name'].count('б')) : acc
+        end
       end
     end
   end
