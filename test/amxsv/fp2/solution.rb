1f445ede718bb8842f8bb02f73283b774aa55ab1
Maksim Slepukhov <maksim.slepukhov@gmail.com>
2016-06-02 17:24:10 +0400

    Implement my each, map, compact, reduce

--- test/amxsv/fp2/solution.rb
+++ test/amxsv/fp2/solution.rb
@@ -6,18 +6,37 @@ module Amxsv
 
       # Написать свою функцию my_each
       def my_each
+        for item in self
+          yield(item)
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        result_arr = MyArray.new
+        my_each do |item|
+          result_arr << yield(item)
+        end
+        result_arr
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        result_arr = MyArray.new
+        my_each do |item|
+          result_arr << item if item
+        end
+        result_arr
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil)
+        acc, *tail = acc.nil? ? self : [acc, *self]
+        for item in tail
+          acc = yield(acc, item)
+        end
+        acc
       end
     end
   end
