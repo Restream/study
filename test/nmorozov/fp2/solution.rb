5ae588e7aad3805dcf31f19b8733ac8cd451e75c
Morozov Nikita <nikmorze@gmail.com>
2016-06-02 09:52:00 +0400

    fp2 homework ver1 has been released

--- test/nmorozov/fp2/solution.rb
+++ test/nmorozov/fp2/solution.rb
@@ -6,18 +6,42 @@ module Nmorozov
 
       # Написать свою функцию my_each
       def my_each
+        for i in self
+          yield(i)
+        end
       end
 
       # Написать свою функцию my_map
       def my_map
+        my_result = MyArray.new([])
+        for i in self
+          my_result << yield(i)
+        end
+        my_result
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        my_result = MyArray.new([])
+        for i in self
+          my_result << i unless i.nil?
+        end
+        my_result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(accumulator = nil)
+        if accumulator.nil?
+          accumulator = self[0]
+          for i in self[1..count - 1]
+            accumulator = yield(accumulator, i)
+          end
+        else
+          for i in self
+            accumulator = yield(accumulator, i)
+          end
+        end
+        accumulator
       end
     end
   end
--- test/nmorozov/fp2/test.rb
+++ test/nmorozov/fp2/test.rb
@@ -10,26 +10,22 @@ class Nmorozov::Fp2Test < Minitest::Test
   end
 
   def test_my_each
-    skip
     result = []
     my_result = []
 
     func = -> (element) { result << element if element.odd? }
     my_func = -> (element) { my_result << element if element.odd? }
-
     assert @array.each(&func) == @my_array.my_each(&my_func)
     assert result == my_result
   end
 
   def test_my_map
-    skip
     func = -> (element) { element * @int }
     assert @array.map(&func) == @my_array.my_map(&func)
     assert @array.map(&func).map(&func) == @my_array.my_map(&func).my_map(&func)
   end
 
   def test_my_compact
-    skip
     func = -> (element) { element if element.even? }
     func_another = -> (element) { element * @int }
     assert @array.map(&func).compact == @my_array.my_map(&func).my_compact
@@ -37,9 +33,7 @@ class Nmorozov::Fp2Test < Minitest::Test
   end
 
   def test_my_reduce
-    skip
     func = -> (acc, element) { acc * element }
-
     assert @array.reduce(&func) == @my_array.my_reduce(&func)
     assert @array.reduce(2, &func) == @my_array.my_reduce(2, &func)
     assert @array.reduce(&:+) == @my_array.my_reduce(&:+)
