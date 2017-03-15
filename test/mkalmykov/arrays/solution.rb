0a799ff73a435fa1ae74de51965a416a62f2d33d
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-15 13:41:23 +0400

    Add arrays task

--- test/mkalmykov/arrays/solution.rb
+++ test/mkalmykov/arrays/solution.rb
@@ -1,12 +1,29 @@
-module Template
+module Mkalmykov
   module Arrays
     class << self
       def replace(array)
-        array
+        max_elem = array.max
+        array.map! { |elem| elem > 0 ? max_elem : elem }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.empty?
+
+        left = 0
+        right = array.length
+
+        while right - left > 1
+          # Не учитываем явно случай, когда длина массива - нечетное число,
+          # потому что осуществляем целочисленное деление, и результат будет
+          # целым числом
+          mid = (left + right) / 2
+          if query < array[mid]
+            right = mid
+          else
+            left = mid
+          end
+        end
+        array[left] == query ? left : -1
       end
     end
   end
--- test/mkalmykov/arrays/test.rb
+++ test/mkalmykov/arrays/test.rb
@@ -1,12 +1,11 @@
 require './test/test_helper.rb'
 require_relative './solution.rb'
 
-class Template::ArraysTest < Minitest::Test
+class Mkalmykov::ArraysTest < Minitest::Test
   # Заменить все положительные элементы целочисленного массива на максимальное значение элементов массива.
   def test_replace
-    skip
     array = [3, 2, -8, 4, 100, -6, 7, 8, -99]
-    new_array = Template::Arrays.replace(array)
+    new_array = Mkalmykov::Arrays.replace(array)
 
     assert new_array == [100, 100, -8, 100, 100, -6, 100, 100, -99]
   end
@@ -14,15 +13,14 @@ class Template::ArraysTest < Minitest::Test
   # Реализовать бинарный поиск
   # Функция должна возвращать индекс элемента
   def test_bin_search
-    skip
-    assert Template::Arrays.search([1], 900) == -1
-    assert Template::Arrays.search([1], 1) == 0
-    assert Template::Arrays.search([], 900) == -1
-    assert Template::Arrays.search([1, 4, 5, 7, 8, 9], 9) == 5
-    assert Template::Arrays.search([1, 4, 5, 7, 8, 9], 1) == 0
-    assert Template::Arrays.search([1, 4, 5, 7, 8, 9], 6) == -1
+    assert Mkalmykov::Arrays.search([1], 900) == -1
+    assert Mkalmykov::Arrays.search([1], 1) == 0
+    assert Mkalmykov::Arrays.search([], 900) == -1
+    assert Mkalmykov::Arrays.search([1, 4, 5, 7, 8, 9], 9) == 5
+    assert Mkalmykov::Arrays.search([1, 4, 5, 7, 8, 9], 1) == 0
+    assert Mkalmykov::Arrays.search([1, 4, 5, 7, 8, 9], 6) == -1
 
     array = (1..10000).to_a
-    assert Template::Arrays.search(array, array[1000]) == 1000
+    assert Mkalmykov::Arrays.search(array, array[1000]) == 1000
   end
 end
